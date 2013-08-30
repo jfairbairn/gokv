5b8b5ac4f4bf1f521cec55a8e85b3550f2961d36
James Fairbairn <james@netlagoon.com>
2013-08-30 09:21:23 +0100

    Moved string ops to string.go

--- gokv.go
+++ gokv.go
@@ -28,36 +28,6 @@ func (s *Store) Close() error {
 	return s.log.Close()
 }
 
-type NotFound struct {
-	key string
-}
-
-func (nf *NotFound) Error() string {
-	return fmt.Sprintf("Nil value found for key %s", nf.key)
-}
-
-func (s *Store) Get(k string) (string, error) {
-	ke := keyError(k)
-	if ke != nil {
-		return "", ke
-	}
-	val := s.data[k]
-	if val == nil {
-		return "", &NotFound{key:k}
-	}
-	return s.data[k].(string), nil
-}
-
-func (s *Store) Put(k string, v string) error {
-	ke := keyError(k)
-	if ke != nil {
-		return ke
-	}
-
-	s.data[k] = v
-	return s.log.Write("PUT", k, v)
-}
-
 func (s *Store) Delete(k string) error {
 	delete(s.data, k)
 	kerr := keyError(k)
---
+++ string.go
@@ -0,0 +1,35 @@
+package gokv
+
+import (
+	"fmt"
+)
+
+type NotFound struct {
+	key string
+}
+
+func (nf *NotFound) Error() string {
+	return fmt.Sprintf("Nil value found for key %s", nf.key)
+}
+
+func (s *Store) Get(k string) (string, error) {
+	ke := keyError(k)
+	if ke != nil {
+		return "", ke
+	}
+	val := s.data[k]
+	if val == nil {
+		return "", &NotFound{key: k}
+	}
+	return s.data[k].(string), nil
+}
+
+func (s *Store) Put(k string, v string) error {
+	ke := keyError(k)
+	if ke != nil {
+		return ke
+	}
+
+	s.data[k] = v
+	return s.log.Write("PUT", k, v)
+}
